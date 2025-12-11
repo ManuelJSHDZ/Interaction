21841d0f8613e30614cc6e349282c6b44fc3669f
ManuelJSHDZ <mjshernandez1111@gmail.com>
2025-12-11 11:31:19 -0600

    Add files via upload

---
+++ java.js
@@ -0,0 +1,13 @@
+const colorBtn = document.getElementById("colorBtn");
+const title = document.getElementById("title");
+
+colorBtn.addEventListener("click", () => {
+  title.style.color = "Green";
+  colorBtn.textContent = "Color Changed!";
+});
+
+const darkModeBtn = document.getElementById("darkModeBtn");
+
+darkModeBtn.addEventListener("click", () => {
+  document.body.classList.toggle("dark-mode");
+});
